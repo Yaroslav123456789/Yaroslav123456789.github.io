0bd51ce8a1e1b8436808768311e924190f59c5b7
Yaroslav123456789 <105737779+Yaroslav123456789@users.noreply.github.com>
2023-03-30 21:40:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function()  {
 	    tg.sendData([{
-		"Id": document.getElementById("Id").value,
 		"Title": document.getElementById("Title").value,
 		"Description": document.getElementById("Description").value,
 		"Price": document.getElementById("Price").value
